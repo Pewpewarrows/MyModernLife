7f53db572b35fb5bc5db05000261314c8eadbbae
Marco Chomut <marco.chomut@gmail.com>
2011-01-26 23:15:45 -0500

    Updated to use my latest and greatest site object paradigm.

--- src/static/js/mymodernlife.js
+++ src/static/js/mymodernlife.js
@@ -5,77 +5,81 @@
     // Since we're in a closure we don't interfere with window.self
     var self = $(this);
 
-    var MML = Site.extend({
-        common: {
-            // This function will fire on every page first
-            init: function() {
-                $('#nav-blog').hover(function() {
-                    $('img', this).attr('src', '/static/images/blog.png');
-                    $('span', this).removeClass('ir');
-                }, function() {
-                    $('img', this).attr('src', '/static/images/blog-grey.png');
-                    $('span', this).addClass('ir');
-                });
-                
-                $('#nav-portfolio').hover(function() {
-                    $('img', this).attr('src', '/static/images/portfolio.png');
-                    $('span', this).removeClass('ir');
-                }, function() {
-                    $('img', this).attr('src', '/static/images/portfolio-grey.png');
-                    $('span', this).addClass('ir');
-                });
-                
-                $('#nav-activity').hover(function() {
-                    $('img', this).attr('src', '/static/images/activity.png');
-                    $('span', this).removeClass('ir');
-                }, function() {
-                    $('img', this).attr('src', '/static/images/activity-grey.png');
-                    $('span', this).addClass('ir');
-                });
-                
-                $('#nav-resume').hover(function() {
-                    $('img', this).attr('src', '/static/images/resume.png');
-                    $('span', this).removeClass('ir');
-                }, function() {
-                    $('img', this).attr('src', '/static/images/resume-grey.png');
-                    $('span', this).addClass('ir');
-                });
-                
-                $('#nav-contact').hover(function() {
-                    $('img', this).attr('src', '/static/images/contact.png');
-                    $('span', this).removeClass('ir');
-                }, function() {
-                    $('img', this).attr('src', '/static/images/contact-grey.png');
-                    $('span', this).addClass('ir');
-                });
-                
-                $('#nav-about').hover(function() {
-                    $('img', this).attr('src', '/static/images/about-me.png');
-                    $('span', this).removeClass('ir');
-                }, function() {
-                    $('img', this).attr('src', '/static/images/about-me-grey.png');
-                    $('span', this).addClass('ir');
-                });
-            }
-        },
-
-        blog: {
-            init: function() {
+    var MML = {
+        ready: {
+            common: {
+                // This function will fire on every page first
+                init: function() {
+                    $('#nav-blog').hover(function() {
+                        $('img', this).attr('src', '/static/images/blog.png');
+                        $('span', this).removeClass('ir');
+                    }, function() {
+                        $('img', this).attr('src', '/static/images/blog-grey.png');
+                        $('span', this).addClass('ir');
+                    });
+                    
+                    $('#nav-portfolio').hover(function() {
+                        $('img', this).attr('src', '/static/images/portfolio.png');
+                        $('span', this).removeClass('ir');
+                    }, function() {
+                        $('img', this).attr('src', '/static/images/portfolio-grey.png');
+                        $('span', this).addClass('ir');
+                    });
+                    
+                    $('#nav-activity').hover(function() {
+                        $('img', this).attr('src', '/static/images/activity.png');
+                        $('span', this).removeClass('ir');
+                    }, function() {
+                        $('img', this).attr('src', '/static/images/activity-grey.png');
+                        $('span', this).addClass('ir');
+                    });
+                    
+                    $('#nav-resume').hover(function() {
+                        $('img', this).attr('src', '/static/images/resume.png');
+                        $('span', this).removeClass('ir');
+                    }, function() {
+                        $('img', this).attr('src', '/static/images/resume-grey.png');
+                        $('span', this).addClass('ir');
+                    });
+                    
+                    $('#nav-contact').hover(function() {
+                        $('img', this).attr('src', '/static/images/contact.png');
+                        $('span', this).removeClass('ir');
+                    }, function() {
+                        $('img', this).attr('src', '/static/images/contact-grey.png');
+                        $('span', this).addClass('ir');
+                    });
+                    
+                    $('#nav-about').hover(function() {
+                        $('img', this).attr('src', '/static/images/about-me.png');
+                        $('span', this).removeClass('ir');
+                    }, function() {
+                        $('img', this).attr('src', '/static/images/about-me-grey.png');
+                        $('span', this).addClass('ir');
+                    });
+                }
             },
-            view_post: function() {
-                window.addthis_config = {
-                    username: 'pewpewarrows',
-                    data_track_clickback: true
-                };
 
-                $.getScript('http://s7.addthis.com/js/250/addthis_widget.js?domready=1');
+            blog: {
+                init: function() {
+                },
+                view_post: function() {
+                    window.addthis_config = {
+                        username: 'pewpewarrows',
+                        data_track_clickback: true
+                    };
+
+                    $.getScript('http://s7.addthis.com/js/250/addthis_widget.js?domready=1');
+                }
             }
         }
-    });
+    };
+
+    window.MML = MML;
 	
     /* DOM Ready */
 	$(function() {
-        window.MML = new MML();
+        Site.init(MML);
 
 	    $('#delete-blog').click(function() {
 		    if (confirm('Are you sure you want to delete this blog?')) {
--- src/static/js/utils.js
+++ src/static/js/utils.js
@@ -124,27 +124,34 @@
  * Essentially, just add 'data-controller' and 'data-action' attributes to the 'body' tag
  * and define those as functions within objects here to get page-specific 'document.ready'
  * code to fire. Big thanks to Paul Irish and Jason Garber.
+ *
+ * Modified to accept the namespace as an init argument and to place all DOM
+ * ready functions in their own inner namespace: 'ready'.
  */
-var Site = Class.extend({
+var Site = {
+    ns: null,
+
     exec: function(controller, action) {
         action = (action === undefined) ? 'init' : action;
 
-        if ((controller !== '') && this[controller] && (typeof this[controller][action] === 'function')) {
-            this[controller][action]();
+        if ((controller !== '') && this.ns.ready[controller] && (typeof this.ns.ready[controller][action] === 'function')) {
+            this.ns.ready[controller][action]();
         }
     },
 
-    init: function() {
+    init: function(ns) {
         var body = document.body,
         controller = body.getAttribute('data-controller'),
         action = body.getAttribute('data-action');
+
+        this.ns = ns;
 
         this.exec('common');
         this.exec(controller);
         this.exec(controller, action);
         this.exec('common', 'finalize');
     }
-});
+};
 
 /*
 var g_condition;
